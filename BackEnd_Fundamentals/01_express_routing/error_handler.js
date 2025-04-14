f0b0a14a2e6a4942cc82d9a325016f04a9bb2fb3
AggelosQubit <aggelosqubit@gmail.com>
2025-04-14 21:47:53 +0200

    Delete unused express routing files

--- BackEnd_Fundamentals/01_express_routing/error_handler.js
+++
@@ -1,6 +0,0 @@
-const errorHandler = (err, req, res, next) => {
-    console.error("🔥 ERROR:", err);//or Logging Mechanic
-    res.status(400).json({ Err: err.message })
-}
-
-module.exports = errorHandler;
